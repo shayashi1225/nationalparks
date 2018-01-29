4284cd1dda44668c4a3f15c85a8efbcac483fab1
shayashi1225 <33945075+shayashi1225@users.noreply.github.com>
2018-01-29 16:05:14 +0700

    Update BackendController.java

--- src/main/java/com/openshift/evg/roadshow/parks/rest/BackendController.java
+++ src/main/java/com/openshift/evg/roadshow/parks/rest/BackendController.java
@@ -17,5 +17,5 @@ public class BackendController{
 
     @RequestMapping(method = RequestMethod.GET, value = "/", produces = "application/json")
     public Backend get() {
-  return new Backend("nationalparks","World Parks", new Coordinates("47.039304", "14.505178"), 4);    }
+  return new Backend("nationalparks","World Parks", new Coordinates("47.039305", "14.505178"), 4);    }
 }
